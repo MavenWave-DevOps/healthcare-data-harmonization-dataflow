b609b892e17fecf341f5293c7a241ac3a951a640
Phil Businger <phil.businger@mavenwave.com>
2022-03-21 16:18:21 -0400

    point build_deps to fork

--- build_deps.sh
+++ build_deps.sh
@@ -39,11 +39,11 @@ rm -rf "${WORK_DIR}"
 mkdir -p "${WORK_DIR}"
 
 # At this point, we require OpenJDK 11.
-readonly JNI_DIR="/usr/lib/jvm/java-11-openjdk-amd64/include"
+readonly JNI_DIR="/usr/local/opt/openjdk@11/include"
 readonly JNI_DIR_LINUX="/usr/lib/jvm/java-11-openjdk-amd64/include/linux"
-
+echo $JNI_DIR
 # Data harmonization repo.
-readonly DH_REPO="${DH_REPO:-https://github.com/GoogleCloudPlatform/healthcare-data-harmonization.git}"
+readonly DH_REPO="${DH_REPO:-https://github.com/Mavenwave-DevOps/healthcare-data-harmonization.git}"
 
 if [[ ! -d "${JNI_DIR}" || ! -d "${JNI_DIR_LINUX}" ]]; then
   echo "Please make sure OpenJDK 11 is installed. On Debian/Ubuntu, run: sudo apt install openjdk-11-jdk"
@@ -77,21 +77,21 @@ export C_INCLUDE_PATH="${JNI_DIR}:${JNI_DIR_LINUX}"
 
 echo "Building wrapping..."
 cd _wrapping
-go mod init github.com/GoogleCloudPlatform/healthcare-data-harmonization/mapping_engine/wrapping
-go mod edit -replace github.com/GoogleCloudPlatform/healthcare-data-harmonization/mapping_language=../../mapping_language
-go mod edit -replace github.com/GoogleCloudPlatform/healthcare-data-harmonization/mapping_engine/proto=../proto
-go mod edit -replace github.com/GoogleCloudPlatform/healthcare-data-harmonization/mapping_engine/util=../util
-go mod edit -replace github.com/GoogleCloudPlatform/healthcare-data-harmonization/mapping_engine=../
-go mod edit -replace github.com/GoogleCloudPlatform/healthcare-data-harmonization/mapping_language/transpiler=../../mapping_language/transpiler
-go mod edit -replace github.com/GoogleCloudPlatform/healthcare-data-harmonization/mapping_engine/transform=../transform
+go mod init github.com/MavenWave-DevOps/healthcare-data-harmonization/mapping_engine/wrapping
+go mod edit -replace github.com/MavenWave-DevOps/healthcare-data-harmonization/mapping_language=../../mapping_language
+go mod edit -replace github.com/MavenWave-DevOps/healthcare-data-harmonization/mapping_engine/proto=../proto
+go mod edit -replace github.com/MavenWave-DevOps/healthcare-data-harmonization/mapping_engine/util=../util
+go mod edit -replace github.com/MavenWave-DevOps/healthcare-data-harmonization/mapping_engine=../
+go mod edit -replace github.com/MavenWave-DevOps/healthcare-data-harmonization/mapping_language/transpiler=../../mapping_language/transpiler
+go mod edit -replace github.com/MavenWave-DevOps/healthcare-data-harmonization/mapping_engine/transform=../transform
 
 go get google.golang.org/genproto/googleapis/api/annotations@v0.0.0-20200526211855-cb27e3aa2013
 go get google.golang.org/genproto/googleapis/rpc/status@v0.0.0-20200526211855-cb27e3aa2013
 go get google.golang.org/grpc/binarylog/grpc_binarylog_v1@v1.27.1
 go get google.golang.org/protobuf/testing/protocmp@v1.26.0
-go get github.com/GoogleCloudPlatform/healthcare-data-harmonization/mapping_engine/proto
-go get github.com/GoogleCloudPlatform/healthcare-data-harmonization/mapping_engine/transform
-go get github.com/GoogleCloudPlatform/healthcare-data-harmonization/mapping_language/transpiler@v0.0.0-20210315190620-fb0f05814962
+go get github.com/MavenWave-DevOps/healthcare-data-harmonization/mapping_engine/proto
+go get github.com/MavenWave-DevOps/healthcare-data-harmonization/mapping_engine/transform
+go get github.com/MavenWave-DevOps/healthcare-data-harmonization/mapping_language/transpiler@v0.0.0-20210315190620-fb0f05814962
 go get google.golang.org/protobuf/encoding/prototext
 go build -ldflags "-s -w" -o "${OUTPUT_DIR}/libwhistler.so" -buildmode=c-shared
 
